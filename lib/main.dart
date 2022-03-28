1225a1e9ccb6e2a8263bb6fc5b514899f4beb1de
cracked2626 <harsh.lohia.mc19@nsut.ac.in>
2022-03-28 21:10:16 +0530

    sdfsdf

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Happy',
+      debugShowCheckedModeBanner: false
+      ,
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,3 +1,7 @@
+
+
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -10,32 +14,82 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return SafeArea(
+      child: Scaffold(
 
-      body: ListView(
+        body: ListView(
+          padding: const EdgeInsets.all(20.0),
 
 
-        children: <Widget>[
-           const Text(
-            'You have pushed the button this many times:',
+          children: <Widget>[
+             const Text(
+              'Hi, User ',
+               style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.w500,),
+            ),
+            const SizedBox(height: 10.0,),
+            const Text(
+              'Your Happiness Index ',
+               style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.w500,),
+            ),const SizedBox(height: 30.0,),
+            Container(
+              decoration: const BoxDecoration(
+                color: Color(0xff00C35A),
+                shape: BoxShape.circle,
+                // borderRadius: BorderRadius.circular(20.0)
+              ),
+
+              padding: const EdgeInsets.all(20.0),
+              child:Container(
+                padding: const EdgeInsets.all(20.0),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                  // borderRadius: BorderRadius.circular(20.0)
+                ),
+
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children:  [
+              Center(
+                   child: Text('😃',style: TextStyle(fontSize: 35.0,fontWeight: FontWeight.w700,),),
+                    ),SizedBox(height: 10.0,),Center(
+                   child: Text('100',style: TextStyle(fontSize: 35.0,fontWeight: FontWeight.w700,),),
+                    ),SizedBox(height: 10.0,),
+              Center(
+                   child: Text('Hi',style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.w700,),),
+                    ),
+
+
+            ],
           ),
-          Text(
-            '$_counter',
-            style: Theme.of(context).textTheme.headline4,
-          ),
-        ],
+              ) ,
+            ),SizedBox(height: 30.0,),
+            Card(
+              elevation: 10,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Center(child: Text('Tell us about Today 😃',style: TextStyle(fontSize: 25.0,fontWeight: FontWeight.w700,color: Colors.black,))),
+                    const SizedBox(height: 30.0,),
+                    TextButton(onPressed: () {  },
+                    child:const Text('Write about Today!',style: TextStyle(fontSize: 25.0,color: Colors.black,fontWeight: FontWeight.w500,) ,)),TextButton(onPressed: () {  },
+                    child:const Text('Record Audio',style: TextStyle(fontSize: 25.0,color: Colors.black,fontWeight: FontWeight.w500,)  ,)),TextButton(onPressed: () {  },
+                    child:const Text('Record Video',style: TextStyle(fontSize: 25.0,color: Colors.black,fontWeight: FontWeight.w500,)  ,)),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+
+       // This trailing comma makes auto-formatting nicer for build methods.
       ),
-     // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
